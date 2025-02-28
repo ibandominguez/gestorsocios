f112ab2673eea00466749191142578d748e8067c
Ibán Domínguez <domingueznoda@gmail.com>
2025-02-28 08:58:46 +0000

    Added new components

---
+++ src/components/Form.tsx
@@ -0,0 +1,96 @@
+import { ReactElement, useEffect, useState } from "react";
+import Button from "./ui/button/Button";
+import Input, { InputProps } from "./Input";
+import moment from "moment";
+
+export type FormField<T> = Omit<InputProps, "onChange" | "name" | "type"> & {
+  name: keyof T;
+  type?:
+    | "text"
+    | "number"
+    | "date"
+    | "time"
+    | "email"
+    | "password"
+    | "tel"
+    | "url"
+    | "search"
+    | "color"
+    | "range"
+    | "checkbox"
+    | "radio"
+    | "file"
+    | "hidden"
+    | "image"
+    | "month"
+    | "week"
+    | "datetime-local";
+};
+
+export interface FormProps<T> {
+  fields: FormField<T>[];
+  buttonTitle: string;
+  buttonIcon?: string;
+  className?: string;
+  inputWrapperClassName?: string;
+  onChange?: (data: T) => void;
+  onSubmit?: (data: T) => void;
+}
+
+export default function Form<T>({
+  fields,
+  buttonTitle,
+  className,
+  inputWrapperClassName,
+  onChange,
+  onSubmit,
+}: FormProps<T>): ReactElement {
+  const [data, setData] = useState<T>({} as T);
+
+  const setDataKey = (key: keyof T, value: string | number | undefined) => {
+    setData((data) => ({ ...data, [key]: value }));
+  };
+
+  useEffect(() => {
+    fields.forEach((field) => {
+      let finalValue = field.value;
+      if (field.type === "date") {
+        finalValue = moment(field.value).format("YYYY-MM-DD");
+      }
+      setDataKey(field.name, finalValue);
+    });
+  }, [fields]);
+
+  useEffect(() => {
+    if (onChange) onChange(data);
+  }, [data]);
+
+  return (
+    <form
+      className={className}
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit && onSubmit(data);
+      }}
+    >
+      <div className={inputWrapperClassName}>
+        {fields.map((field, index) => (
+          <Input
+            key={index}
+            className="text-sm mb-3"
+            {...field}
+            name={field.name as string}
+            value={data[field.name] as string | number | undefined}
+            onChange={(value) =>
+              setDataKey(
+                field.name,
+                field.type === "number" ? parseFloat(value as string) : value,
+              )
+            }
+          />
+        ))}
+      </div>
+      {onSubmit && <Button>{buttonTitle}</Button>}
+    </form>
+  );
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,81 @@
+import React, { ReactElement, InputHTMLAttributes } from 'react'
+
+export interface InputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  name: string
+  label?: string
+  className?: string
+  inputClassName?: string
+  required?: boolean
+  placeholder?: string
+  type?:
+    | 'text'
+    | 'number'
+    | 'date'
+    | 'time'
+    | 'email'
+    | 'password'
+    | 'tel'
+    | 'url'
+    | 'search'
+    | 'color'
+    | 'range'
+    | 'checkbox'
+    | 'radio'
+    | 'file'
+    | 'hidden'
+    | 'image'
+    | 'month'
+    | 'week'
+    | 'datetime-local'
+    | 'select'
+    | 'textarea'
+  value?: string | number
+  options?: { name: string; value: string | number }[]
+  onChange: (value: string | number) => void
+}
+
+export default function Input(props: InputProps): ReactElement {
+  const inputClassName =
+    'p-2 rounded-md shadow-md bg-white w-full ' + props.inputClassName
+
+  return (
+    <div className={`p-1 ${props.className}`}>
+      {props.label && (
+        <label className="block w-full text-sm text-gray-500">
+          {props.label}
+        </label>
+      )}
+      {props.type === 'select' ? (
+        <select
+          name={props.name}
+          value={props.value || ''}
+          required={props.required}
+          className={inputClassName}
+          onChange={(event) => props.onChange(event.target.value)}
+        >
+          {Boolean(props.placeholder) && (
+            <option value="" disabled>
+              {props.placeholder}
+            </option>
+          )}
+          {props.options?.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input
+          name={props.name}
+          value={props.value || ''}
+          required={props.required}
+          placeholder={props.placeholder}
+          type={props.type}
+          className={inputClassName}
+          onChange={(event) => props.onChange(event.target.value)}
+        />
+      )}
+    </div>
+  )
+}

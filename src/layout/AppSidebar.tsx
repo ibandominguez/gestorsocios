f4e71590e8190e1380f706baaa45372959ef337d
Ibán Domínguez <domingueznoda@gmail.com>
2025-02-28 11:06:25 +0000

    Fixed logo and removed unsued links

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -326,28 +326,13 @@ const AppSidebar: React.FC = () => {
       >
         <Link to="/">
           {isExpanded || isHovered || isMobileOpen ? (
-            <>
-              <img
-                className="dark:hidden"
-                src="/images/logo/logo.svg"
-                alt="Logo"
-                width={150}
-                height={40}
-              />
-              <img
-                className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
-                alt="Logo"
-                width={150}
-                height={40}
-              />
-            </>
+            <img className="w-full" src="/images/logo/logo.png" alt="Logo" />
           ) : (
             <img
-              src="/images/logo/logo-icon.svg"
+              src="/images/logo/logo.png"
               alt="Logo"
-              width={32}
-              height={32}
+              width={40}
+              height={40}
             />
           )}
         </Link>

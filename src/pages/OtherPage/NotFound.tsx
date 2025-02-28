ab46be9275c1f9d1cb5b8d7acd0389ec12bb1662
Ibán Domínguez <domingueznoda@gmail.com>
2025-02-28 13:27:37 +0000

    Fixed 404 page

--- src/pages/OtherPage/NotFound.tsx
+++ src/pages/OtherPage/NotFound.tsx
@@ -1,6 +1,8 @@
 import GridShape from "../../components/common/GridShape";
 import { Link } from "react-router";
 import PageMeta from "../../components/common/PageMeta";
+import image404 from "../../../public/images/error/404.svg";
+import image404Dark from "../../../public/images/error/404-dark.svg";
 
 export default function NotFound() {
   return (
@@ -10,21 +12,16 @@ export default function NotFound() {
         description="This is React.js 404 Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <div className="relative flex flex-col items-center justify-center min-h-screen p-6 overflow-hidden z-1">
-        <GridShape />
         <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
           <h1 className="mb-8 font-bold text-gray-800 text-title-md dark:text-white/90 xl:text-title-2xl">
             ERROR
           </h1>
 
-          <img src="/images/error/404.svg" alt="404" className="dark:hidden" />
-          <img
-            src="/images/error/404-dark.svg"
-            alt="404"
-            className="hidden dark:block"
-          />
+          <img src={image404} alt="404" className="dark:hidden" />
+          <img src={image404Dark} alt="404" className="hidden dark:block" />
 
           <p className="mt-10 mb-6 text-base text-gray-700 dark:text-gray-400 sm:text-lg">
-            We can’t seem to find the page you are looking for!
+            La página que buscas no existe
           </p>
 
           <Link
